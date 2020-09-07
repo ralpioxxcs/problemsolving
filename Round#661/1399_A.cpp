551f80409b334ed69308af96302a0c92d89c108f
Cnux <52454344+ralpioxxcs@users.noreply.github.com>
2020-09-07 21:49:07 +0900

    foramt selection

--- Round#661/1399_A.cpp
+++ Round#661/1399_A.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 #define TEST
 
 int main() {
-	std::cin.tie(0)->sync_with_stdio(0);
-	std::cout.tie(0);
+  std::cin.tie(0)->sync_with_stdio(0);
+  std::cout.tie(0);
 #ifdef TEST
-	freopen("input.txt", "r", stdin);
+  freopen("input.txt", "r", stdin);
 #endif
-	int T;
-    std::string ans;
-    int n; // (1<=n<=50)
-	std::cin >> T;
-	 while (T--) {
-         std::cin >> n;
-         std::vector<int> a(n);
-         for(int i=0; i<n;i++) {
-             std::cin >> a[i];
-         }
-         sort(a.begin(),a.end());
-         int min_val = 101;
-         for(int i=0; i<n-1; i++) {
-             if(abs(a[i]-a[i+1]) <=1) {
-                 continue;
-             } else {
-                 std::cout<<"NO\n";
-                 break;
-             }
-         }
-		std::cout <<"YES\n";
-	}
-	return 0;
+  int T;
+  std::string ans;
+  int n;  // (1<=n<=50)
+  std::cin >> T;
+  while (T--) {
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      std::cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int min_val = 101;
+    for (int i = 0; i < n - 1; i++) {
+      if (abs(a[i] - a[i + 1]) <= 1) {
+        continue;
+      } else {
+        std::cout << "NO\n";
+        break;
+      }
+    }
+    std::cout << "YES\n";
+  }
+  return 0;
 }
